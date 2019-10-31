77b4a4867c3d9a0a89051f71a6046c78d1c0ccdd
sharon story smith <sstorysmith@gmail.com>
2019-10-31 15:43:58 -0500

    complete set of code

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (id, burger_name, devoured) VALUES ('double bacon cheeseburger', 'false');
+INSERT INTO burgers (id, burger_name, devoured) VALUES ('single patty melt', 'false');
+INSERT INTO burgers (id, burger_name, devoured) VALUES ('dq_dude', 'false');
